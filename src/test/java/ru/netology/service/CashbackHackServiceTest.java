6e23ee349b7dd93d7f699d62c367d310a5a6878f
Максим Бокарев <unclemaxm@gmail.com>
2022-04-23 12:29:08 +0400

    junit4 commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,16 +1,26 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         CashbackHackService cashback = new CashbackHackService();
         int amount = 900;
 
         int actual = cashback.remain(amount);
         int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void boughtExactly1000() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashback.remain(amount);
+        int expected = 0;
         assertEquals(actual, expected);
     }
 }
