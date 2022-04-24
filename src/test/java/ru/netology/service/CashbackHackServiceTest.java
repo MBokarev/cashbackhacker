53b179d52edbb7c0f62485d463fa1342e9d1fa4a
Максим Бокарев <unclemaxm@gmail.com>
2022-04-24 20:41:45 +0400

    fourth commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -13,4 +13,14 @@ public class CashbackHackServiceTest {
         int expected = 100;
         assertEquals(actual, expected);
     }
+
+    @org.testng.annotations.Test
+    public void boughtExactly1000() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashback.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
 }
